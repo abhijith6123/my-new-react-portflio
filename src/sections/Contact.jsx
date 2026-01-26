7516d553eca840279fecbba4cfc900cea15b72fa
Deepak B <crazydeepak3657@gmail.com>
2026-01-26 14:04:02 +0530

    first commit

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", "f428a7c0-2282-4b30-a9b3-89bb28670aea");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" required/>
+      <input type="email" name="email" required/>
+      <textarea name="message" required></textarea>
+      <button type="submit">Submit Form</button>
+      <span>{result}</span>
+    </form>
+  );
+}
